5c12f071a2d06e0b355954a1c3676205fafa9110
NOUHAILA ERRAKEB <116765284+nouhaerr@users.noreply.github.com>
2023-07-10 01:15:57 +0100

    Update start_eating.c

--- philo/start_eating.c
+++ philo/start_eating.c
@@ -14,15 +14,22 @@
 
 void	is_taking_fork(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->r_fork);
-	philo->right = 0;
-	pthread_mutex_unlock(&philo->r_fork);
-	pthread_mutex_lock(&philo->l_fork);
-	philo->left = 0;
-	pthread_mutex_unlock(&philo->l_fork);
+	pthread_mutex_lock(philo->r_fork);
+	write_status(philo, "has taken a fork");
+	pthread_mutex_lock(philo->l_fork);
+	write_status(philo, "has taken a fork");
+}
+
+void	put_down_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
 }
 
 void	is_eating(t_philo *philo)
 {
 	is_taking_fork(philo);
+	gettimeofday(&philo->time_of_last_meal, NULL);
+	write_status(philo, "is eating");
+	put_down_forks(philo);
 }
