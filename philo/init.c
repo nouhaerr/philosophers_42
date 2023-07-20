60be014e32370bd3ec87ab92af9702620d54f8f2
NOUHAILA ERRAKEB <116765284+nouhaerr@users.noreply.github.com>
2023-07-20 15:12:05 +0100

    Update init.c

--- philo/init.c
+++ philo/init.c
@@ -27,8 +27,6 @@ int	init_data(char **av, t_data *ph_info)
 	if (!is_empty(av))
 		return (0);
 	ph_info->nbr_of_ph = ft_atoi(av[1]);
-	if (ph_info->nbr_of_ph < 1)
-		return (0);
 	ph_info->t_to_die = ft_atoi(av[2]); // check if < 60
 	ph_info->t_to_eat = ft_atoi(av[3]); // check if < 60 return 1;
 	ph_info->t_to_sleep = ft_atoi(av[4]); // check if < 60
@@ -39,8 +37,8 @@ int	init_data(char **av, t_data *ph_info)
 		if (ph_info->t_of_each_ph_must_eat == 0)
 			return (0);
 	}
-	if (ph_info->t_to_die < 1 || ph_info->t_to_eat < 1 || ph_info->t_to_sleep < 1
-		|| ph_info->t_of_each_ph_must_eat < 0)
+	if (ph_info->nbr_of_ph < 1 || ph_info->t_to_die < 1 || ph_info->t_to_eat < 1
+		|| ph_info->t_to_sleep < 1 || ph_info->t_of_each_ph_must_eat < 0)
 		return (0);
 	if (init_mutex(ph_info))
 		return (0);
@@ -53,21 +51,21 @@ int	init_philo(t_philo *philo, t_data data)
 	int	i;
 
 	i = 0;
-	philo->meals = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	philo->count_meals = (int *)malloc(sizeof(int));
+	philo->meals = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * data.nbr_of_ph);
+	philo->count_meals = (int *)malloc(sizeof(int) * data.nbr_of_ph);
 	if (!philo->meals || !philo->count_meals)
 		return (printf("Allocation Failed !!\n"));
-	pthread_mutex_init(philo->meals, NULL);
 	philo->count_meals[0] = 0;
 	while (i < data.nbr_of_ph)
 	{
 		if (pthread_mutex_init(&data.forks[i], NULL) != 0)
 			return (1);
+		pthread_mutex_init(&philo->meals[i], NULL);
 		philo[i].philo_inf = data;
 		philo[i].ph_id = i + 1;
 		philo[i].time_of_last_meal = ft_gettime();
 		philo[i].meals = philo->meals;
-		philo[i].count_meals = philo[0].count_meals;
+		philo[i].count_meals = &philo->count_meals[0];
 		philo[i].r_fork = &data.forks[i];
 		philo[i].l_fork = &data.forks[(i + 1) % data.nbr_of_ph];
 		i++;
